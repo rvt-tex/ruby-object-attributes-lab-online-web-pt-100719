3ba26c0e618dfe1ecfbda3cb08a3a02ccee9ff0b
Ruel Tingling <rvtingling@gmail.com>
2019-10-13 00:16:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,73 @@
+#class Dog
+	#def name=(dogs_name)
+	    #@this_dogs_name = dogs_name
+	#end
+	 
+  #def name
+	    #@this_dogs_name
+	#end
+	
+	#def breed=(dogs_breed)
+	    #@this_dogs_breed = dogs_breed
+	#end
+	 
+  #def breed
+	    #@this_dogs_breed
+	#end
+#end
+	
+#fido = Dog.new
+#fido.name = "Fido"
+	 
+#puts fido.name
+#puts fido.breed
+	
+	
+#class Dog
+  #def name=(fido)
+    #@dogs_name = fido
+  #end
+
+  #def name
+    #@dogs_names
+  #end
+
+  #def breed=(beagle)
+    #@breed = beagle
+  #end
+
+  #def breed
+    #@breed
+  #end
+#end
+
+
+#fido = Dog.new
+#fido.name = "Fido"
+
+#puts fido.name
+
+#snoopy = Dog.new
+#snoopy.breed = "Beagle"
+
 class Dog
-	def name=(dogs_name)
-	    @this_dogs_name = dogs_name
-	end
-	 
+  def name=(name)
+    @name = name
+  end
+
   def name
-	    @this_dogs_name
-	end
-	
-	def breed=(dogs_breed)
-	    @this_dogs_breed = dogs_breed
-	end
-	 
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
   def breed
-	    @this_dogs_breed
-	end
-	
+    @breed
+  end
 end
-	
-	
-fido = Dog.new
-fido.name = "Fido"
-	 
-puts fido.name
-puts fido.breed
+
+
 	
 	
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,4 @@
-class Person
- 
+#class Person
   #def initialize(first_name, last_name)
     #@first_name = first_name
     #@last_name = last_name
@@ -14,33 +13,39 @@ class Person
   #def name
     #"#{@first_name} #{@last_name}".strip
   #end
+
   
-  def initialize(name)
-      @name = name
+ #def job=(person_job)
+	    #@this_person_job = person_job
+	#end
+	 
+  #def job
+	   # @this_person_job
+	#end
+#end
+
+#john_brown = Person.new
+#john_brown.name = "john brown"
+	 
+#john_brown.name
+#john_brown.job
+
+
+class Person
+  def name=(name)
+    @name = name
   end
-	 
-	def name
-	    @name
-	end
 
-  def name=(new_name)
-	    @name = new_name
-	end
-  
-  
- def job=(person_job)
-	    @this_person_job = person_job
-	end
-	 
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
   def job
-	    @this_person_job
-	end
-
+    @job
+  end
 end
-
-john = Person.new("john")
-john.name = "john"
-	 
-john.name
-john.job
 
